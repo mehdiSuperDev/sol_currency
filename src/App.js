91b8371c00db89eb0e5cc582f7e5e8c402015f17
Mehdi Raheemun <mraheemu@student.42.fr>
2022-03-22 21:08:00 +0100

    connecting with phantom wallet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
  
 const App = () => {
+    const [walletConnected,setWalletConnected] = useState(false);
+    const [provider, setProvider] = useState();
+    const [loading, setLoading] = useState();
+
+    const getProvider = async () => {
+        if ("solana" in window) {
+           const provider = window.solana;
+           if (provider.isPhantom) {
+              return provider;
+           }
+        } else {
+           window.open("https://www.phantom.app/", "_blank");
+        }
+     };
+     
+     const walletConnectionHelper = async () => {
+        if (walletConnected){
+           //Disconnect Wallet
+           setProvider();
+           setWalletConnected(false);
+        } else {
+           const userWallet = await getProvider();
+           if (userWallet) {
+              await userWallet.connect();
+              userWallet.on("connect", async () => {
+                 setProvider(userWallet);
+                 setWalletConnected(true);
+              });
+           }
+        }
+     }
+
    return (
        <div>
            <h1>Create your own token using JavaScript</h1>
+           {
+            walletConnected?(
+                <p><strong>Public Key:</strong> {provider.publicKey.toString()}</p>                   
+                ):<p></p>
+            }
+        
+            <button onClick={walletConnectionHelper} disabled={loading}>
+                {!walletConnected?"Connect Wallet":"Disconnect Wallet"}
+            </button> 
        </div>
    )
 };
